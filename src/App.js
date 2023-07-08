06396f0f400789cce6d61c3b08e2d252d55c0edd
Dra <nd.mirica@gmail.com>
2023-07-08 15:59:49 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import './App.css';
 
 function App() {
+
+  //Random password generator
+  const GenerateRandomPassword = (length) => {
+    //characters being used
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@!#%^&*$";
+
+    //container for the final string
+    let str = '';
+
+    //for loop to pick the characters
+    for(let i = 0; i < length; i++){
+      str += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return str;
+
+  }
+
+  const header = (
+      <header>
+        <h1>
+          Password Generator 
+        </h1>
+        <h2>
+          Random, Secure and Free
+        </h2>
+      </header>
+  );
+
   return (
     <div className="App">
-      
+      {/* START Header */}
+      {header}
+      {/* END Header */}
+
+      {/* START Main */}
+      <main>
+
+      </main>
+      {/* END Main */}
+
     </div>
   );
+
+
 }
 
 export default App;
