2c0f7f9a0983d32f91d292663b7b7e2e3a193852
Dra <nd.mirica@gmail.com>
2023-07-10 12:40:31 +0300

    header to componenr

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header>
+      <h1>Password Generator</h1>
+      <h2>Random, Secure and Free</h2>
+    </header>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Header } from "./components/Header";
 
 //Main component
 const PageContainer = () => {
   return (
     //React.Fragment return an element without using a html element, empty <> </> can also be used
     <React.Fragment>
-      
+      <Header />
     </React.Fragment>
   );
 };
+
 
 //Obtain the main div for the app from the main page
 const root = ReactDOM.createRoot(document.getElementById("root"));
